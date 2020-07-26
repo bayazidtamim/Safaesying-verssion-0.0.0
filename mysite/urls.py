f13cf8a95361afb230dc9ccc9adb0d26a349eb8a
Tamim <bayazidtamim28@gmail.com>
2020-07-26 16:56:51 +0600

    wdww

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from blog.views.register import RegisterView
- 
+from blog.views.about import about
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -26,6 +26,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='blog:home')),
     path('register/', RegisterView.as_view(), name='register'),
-    path('', include('django.contrib.auth.urls'))
-     
+    path('', include('django.contrib.auth.urls')),
+    path('/about',about, name='about'), 
 ]
